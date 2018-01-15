b00cc4d73c185cedc68abc82995914bd1b06c094
Alan <1172632508@qq.com>
2018-01-15 15:22:28 +0800

    initialize project

---
+++ ALActionSheet/ALActionSheet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ALActionSheet
+//
+//  Created by Alan on 2018/1/15.
+//  Copyright © 2018年 Alan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ALActionSheet/ALActionSheet/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ALActionSheet
+//
+//  Created by Alan on 2018/1/15.
+//  Copyright © 2018年 Alan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
